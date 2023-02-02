d7e0ad3859da30553a5fa7e8f9a37d4b8c9f31e9
Chandra Sekhar Alluri <allurichandu9@gmail.com>
2023-02-01 22:30:04 -0500

    React - Customer reward points application

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# customer-reward-points
-this is for customer reward points
+Please visit: https://youtu.be/KwGQFvd9N1s  for a demo
+
+A retailer offers a rewards program to its customers, awarding points based on each recorded purchase.
+
+A customer receives 2 points for every dollar spent over $100 in each transaction, plus 1 point for every dollar spent between $50 and $100 in each transaction.
+
+(e.g., a $120 purchase = 2x$20 + 1x$50 = 90 points).
+
+ 
+Given a record of every transaction during a three-month period, calculate the reward points earned for each customer per month and total.
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br>
+You will also see any lint errors in the console.
+
+
---
+++ src/service/dataService.js
@@ -0,0 +1,135 @@
+
+
+export default function() {
+  // simulates data coming from a database.
+  return Promise.resolve(
+    [
+        {
+          custid: 1,
+          name: "David",
+          amt: 120,
+          transactionDt: "10-01-2022"
+        },
+        {
+          custid: 1,
+          name: "David",
+          amt: 75,
+          transactionDt: "10-21-2022"
+        },
+        {
+          custid: 1,
+          name: "David",
+          amt: 94,
+          transactionDt: "10-21-2022"
+        },
+        {
+          custid: 1,
+          name: "David",
+          amt: 10,
+          transactionDt: "11-01-2022"
+        },
+        {
+          custid: 1,
+          name: "David",
+          amt: 75,
+          transactionDt: "11-21-2022"
+        },
+        {
+          custid: 1,
+          name: "David",
+          amt: 200,
+          transactionDt: "11-01-2022"
+        },
+        {
+          custid: 1,
+          name: "David",
+          amt: 1,
+          transactionDt: "12-04-2022"
+        },
+        {
+          custid: 1,
+          name: "David",
+          amt: 80,
+          transactionDt: "12-03-2022"
+        },
+        {
+          custid: 1,
+          name: "David",
+          amt: 224,
+          transactionDt: "12-21-2022"
+        },
+        {
+          custid: 2,
+          name: "John",
+          amt: 125,
+          transactionDt: "10-01-2022"
+        },
+        {
+          custid: 2,
+          name: "John",
+          amt: 75,
+          transactionDt: "10-21-2022"
+        },
+        {
+          custid: 2,
+          name: "John",
+          amt: 10,
+          transactionDt: "11-01-2022"
+        },
+        {
+          custid: 2,
+          name: "John",
+          amt: 75,
+          transactionDt: "11-21-2022"
+        },
+        {
+          custid: 2,
+          name: "John",
+          amt: 200,
+          transactionDt: "12-01-2022"
+        },
+        {
+          custid: 2,
+          name: "John",
+          amt: 224,
+          transactionDt: "12-21-2022"
+        },
+        {
+          custid: 3,
+          name: "Sony",
+          amt: 175,
+          transactionDt: "10-01-2022"
+        },
+        {
+          custid: 3,
+          name: "John",
+          amt: 55,
+          transactionDt: "10-21-2022"
+        },
+        {
+          custid: 3,
+          name: "John",
+          amt: 30,
+          transactionDt: "11-01-2022"
+        },
+        {
+          custid: 3,
+          name: "John",
+          amt: 95,
+          transactionDt: "11-21-2022"
+        },
+        {
+          custid: 3,
+          name: "John",
+          amt: 220,
+          transactionDt: "12-01-2022"
+        },
+        {
+          custid: 3,
+          name: "John",
+          amt: 124,
+          transactionDt: "12-21-2022"
+        },
+    ]
+  );
+};
